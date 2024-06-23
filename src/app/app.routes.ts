24e3335d0fcfdc170a776b36616a73cfbb43060a
KleberMera <klebermera2016@gmail.com>
2024-06-23 12:29:29 -0500

    rutas del auth

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,13 @@
 import { Routes } from '@angular/router';
 
-
-
-
 export const routes: Routes = [
-    {
-        path: 'auth',
-        loadChildren: () => import('./auth/auth.routes')
-    },
-    {
-        path: '',
-        redirectTo: 'auth',
-        pathMatch: 'full'
-    }
- 
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.routes'),
+  },
+  {
+    path: '',
+    redirectTo: 'auth',
+    pathMatch: 'full',
+  },
 ];
