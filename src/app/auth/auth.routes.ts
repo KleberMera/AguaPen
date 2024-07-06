50aad03677d5438e86fdafed15746c564f2fc2e0
KleberMera <klebermera2016@gmail.com>
2024-07-06 18:21:57 -0500

    rutas hijas del login

--- src/app/auth/auth.routes.ts
+++ src/app/auth/auth.routes.ts
@@ -1,18 +1,25 @@
-import { Routes } from "@angular/router";
+import { Routes } from '@angular/router';
 
-export const routes : Routes = [
-    {
-        path: '',
-        loadComponent: () => import('./login/login.component')
-    },
-    {
-        path: 'forgot-password',
-        loadComponent: () => import('./forgot-password/forgot-password.component')
-    },
-    {
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./login/login.component'),
+
+    children: [
+      {
         path: 'login',
-        loadComponent: () => import('./login/login.component')
-    }
-]
+        loadComponent: () => import('./login/login.component'),
+      },
+      {
+        path: 'forgot-password',
+        loadComponent: () => import('./forgot-password/forgot-password.component'),
+      },
+      {
+        path: '**',
+        redirectTo: '/auth/login',
+      }
+    ]
+  },
+];
 
 export default routes;
