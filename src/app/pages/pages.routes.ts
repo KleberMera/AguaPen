e48ff2462b1f6f5a904fe2d66f1d09d04f5cbcdd
apolo <apolocuenta1@gmail.com>
2024-07-25 19:52:25 -0500

    a

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -43,6 +43,7 @@ export const routes: Routes = [
         path: 'regx-area',
         loadComponent: () => import('./regx-area/regx-area.component'),
         
+        
       },
       {
         path: '**',
