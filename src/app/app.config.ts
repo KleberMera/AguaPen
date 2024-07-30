565958c2043f54ed86df17a04553db17de375cf9
KleberMera <klebermera2016@gmail.com>
2024-07-29 20:23:14 -0500

    fix

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,18 +2,21 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
-import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
+import {
+  BrowserAnimationsModule,
+  provideAnimations,
+} from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { AuthInterceptor } from './clients/auth.interceptor';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes),
+  providers: [
+    provideRouter(routes),
     provideAnimations(),
     provideHttpClient(),
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
     BrowserModule,
-    BrowserAnimationsModule, provideAnimationsAsync()
-    
-  ]
+    BrowserAnimationsModule,
+    provideAnimationsAsync(),
+  ],
 };
