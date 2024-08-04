f70cce2d93f737e4a7e08f56d868739b6e32c40f
KleberMera <klebermera2016@gmail.com>
2024-08-04 18:09:34 -0500

    manejando errores

--- src/app/pages/edit-delete/edit-delete.component.ts
+++ src/app/pages/edit-delete/edit-delete.component.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Product } from '../../interfaces/products.interfaces';
 import { MessageService } from 'primeng/api';
+import { DeleteService } from '../../services/delete.service';
 import { RegisterDetailsService } from '../../services/register-details.service';
 
 @Component({
@@ -15,7 +16,7 @@ import { RegisterDetailsService } from '../../services/register-details.service'
   styleUrl: './edit-delete.component.scss',
   providers: [MessageService],
 })
-export default class EditDeleteComponent implements OnInit {
+export default class EditDeleteComponent {
   reportOptions = [
     { label: 'Trabajadores', value: 'trabajadores' },
     { label: 'Areas', value: 'areas' },
@@ -34,20 +35,19 @@ export default class EditDeleteComponent implements OnInit {
   productosOptions: any[] = [];
   selectedNewProduct: any = null;
   newProductQuantity: number = 0;
+  loading: boolean = false;
 
   private srvList = inject(ListService);
+  private messageService = inject(MessageService);
+  private deleteService = inject(DeleteService);
   private registerDetailsService = inject(RegisterDetailsService);
 
-  private messageService = inject(MessageService);
 
   ngOnInit() {
     this.srvList.getlistProducts().subscribe(
       (res) => {
         this.productosOptions = res.data
-          .filter(
-            (product: any) =>
-              product.stock_producto > 0 && product.estado_producto === 1
-          )
+          .filter((product: any) => product.stock_producto > 0 && product.estado_producto === 1)
           .map((product: any) => ({
             label: `${product.nombre_producto} (Código: ${product.codigo_producto})`,
             value: product,
@@ -63,6 +63,32 @@ export default class EditDeleteComponent implements OnInit {
   }
 
 
+
+  onDeleteDetalle(detalleId: number) {
+    this.loading = true;
+    this.deleteService.requestdeletedetalle(detalleId).subscribe(
+      () => {
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Éxito',
+          detail: 'Detalle eliminado exitosamente',
+        });
+        // Remove the deleted detail from registroDetalles array
+        this.registroDetalles = this.registroDetalles.filter(detalle => detalle.id_tbl_registro_detalles !== detalleId);
+        this.resetForm();
+        this.loading = false;
+      },
+      (error) => {
+        this.loading = false;
+        console.error('Error deleting Registro Detalle:', error);
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: 'Hubo un problema al eliminar el detalle del registro',
+        });
+      }
+    );
+  }
 
   onReportChange(event: any) {
     if (this.selectedReport === 'trabajadores') {
@@ -148,13 +174,11 @@ export default class EditDeleteComponent implements OnInit {
   }
 
 
-
   onReplaceProduct() {
+    this.loading = true;
     if (this.selectedNewProduct && this.newProductQuantity > 0) {
-      const originalProductStock =
-        this.selectedProduct.stock_producto + this.selectedProduct.cantidad;
-      const newProductStock =
-        this.selectedNewProduct.stock_producto - this.newProductQuantity;
+      const originalProductStock = this.selectedProduct.stock_producto + this.selectedProduct.cantidad;
+      const newProductStock = this.selectedNewProduct.stock_producto - this.newProductQuantity;
 
       const updatedOriginalProduct = {
         id: this.selectedProduct.id_tbl_productos,
@@ -174,68 +198,75 @@ export default class EditDeleteComponent implements OnInit {
       };
 
       // Update the original product's stock
-      this.registerDetailsService
-        .postEditProductos(updatedOriginalProduct)
-        .subscribe(
-          () => {
-            // Update the new product's stock
-            this.registerDetailsService
-              .postEditProductos(updatedNewProduct)
-              .subscribe(
+      this.registerDetailsService.postEditProductos(updatedOriginalProduct).subscribe(
+        () => {
+          // Update the new product's stock
+          this.registerDetailsService.postEditProductos(updatedNewProduct).subscribe(
+            () => {
+              // Update the registro detalle
+              this.registerDetailsService.postEditRegistroDetalle(updatedRegistroDetalle).subscribe(
                 () => {
-                  // Update the registro detalle
-                  this.registerDetailsService
-                    .postEditRegistroDetalle(updatedRegistroDetalle)
-                    .subscribe(
-                      () => {
-                        this.messageService.add({
-                          severity: 'success',
-                          summary: 'Éxito',
-                          detail: 'Producto reemplazado exitosamente',
-                        });
-                        // Update UI as needed
-                      },
-                      (error) => {
-                        console.error(
-                          'Error updating Registro Detalle:',
-                          error
-                        );
-                        this.messageService.add({
-                          severity: 'error',
-                          summary: 'Error',
-                          detail:
-                            'Hubo un problema al actualizar el detalle del registro',
-                        });
-                      }
-                    );
+                  this.messageService.add({
+                    severity: 'success',
+                    summary: 'Éxito',
+                    detail: 'Producto reemplazado exitosamente',
+                  });
+                  // Update UI as needed
+                  this.resetForm();
+                    this.loading = false;
                 },
                 (error) => {
-                  console.error('Error updating new product stock:', error);
+                  this.loading = false;
+                  console.error('Error updating Registro Detalle:', error);
                   this.messageService.add({
                     severity: 'error',
                     summary: 'Error',
-                    detail:
-                      'Hubo un problema al actualizar el stock del nuevo producto',
+                    detail: 'Hubo un problema al actualizar el detalle del registro',
                   });
                 }
               );
-          },
-          (error) => {
-            console.error('Error updating original product stock:', error);
-            this.messageService.add({
-              severity: 'error',
-              summary: 'Error',
-              detail:
-                'Hubo un problema al actualizar el stock del producto original',
-            });
-          }
-        );
+            },
+            (error) => {
+              this.loading = false;
+              console.error('Error updating new product stock:', error);
+              this.messageService.add({
+                severity: 'error',
+                summary: 'Error',
+                detail: 'Hubo un problema al actualizar el stock del nuevo producto',
+              });
+            }
+          );
+        },
+        (error) => {
+          this.loading = false;
+          console.error('Error updating original product stock:', error);
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail: 'Hubo un problema al actualizar el stock del producto original',
+          });
+        }
+      );
     } else {
+      this.loading = false;
       this.messageService.add({
         severity: 'warn',
         summary: 'Advertencia',
         detail: 'Selecciona un producto y cantidad válida',
       });
     }
+  }
+
+
+  private resetForm() {
+    this.selectedReport = undefined;
+    this.trabajadoresOptions = [];
+    this.selectedTrabajador = undefined;
+    this.idRegistrosOptions = [];
+    this.selectedIdRegistro = undefined;
+    this.registroDetalles = [];
+    this.selectedProduct = null;
+    this.selectedNewProduct = null;
+    this.newProductQuantity = 0;
   }
 }
