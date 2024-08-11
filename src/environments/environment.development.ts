692303a649124134f398add8f359b8e16bd3e48d
apolo <apolocuenta1@gmail.com>
2024-08-11 09:59:32 -0500

    correccion img

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
-aguapenApi: 'https://aguapenrestapi-arvnq.ondigitalocean.app/api/',
-//  aguapenApi: 'http://127.0.0.1:8000/api/',
+//aguapenApi: 'https://aguapenrestapi-arvnq.ondigitalocean.app/api/',
+  aguapenApi: 'http://127.0.0.1:8000/api/',
 
-  imageApi: 'https://aguapenrestapi-arvnq.ondigitalocean.app',
-  //imageApi: 'http://127.0.0.1:8000',
+//  imageApi: 'https://aguapenrestapi-arvnq.ondigitalocean.app',
+  imageApi: 'http://127.0.0.1:8000',
 };
