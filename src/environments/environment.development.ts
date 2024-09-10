3fcbfbb31b5b03d35031798f98e177837450c670
KleberMera <klebermera2016@gmail.com>
2024-09-09 19:27:06 -0500

    a

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -4,8 +4,8 @@ export const environment = {
   //aguapenApi: 'http://192.168.1.30:80/api/',
 
 
-  imageApi: 'https://aguapenrestapi-arvnq.ondigitalocean.app',
-  //imageApi: 'http://127.0.0.1:8000',
+ //imageApi: 'https://aguapenrestapi-arvnq.ondigitalocean.app',
+  imageApi: 'http://127.0.0.1:8000',
   //imageApi: 'http://192.168.1.30:80/',
 
 
