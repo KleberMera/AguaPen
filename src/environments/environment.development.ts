1e629e129a9a440d9a7da1f218e22d7b65d6f571
KleberMera <klebermera2016@gmail.com>
2024-10-19 16:58:08 -0500

    production enviroments

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,10 +1,11 @@
 export const environment = {
   //aguapenApi: 'https://aguapenrestapi-arvnq.ondigitalocean.app/api/',
-  aguapenApi: 'http://127.0.0.1:8000/api/',
+  //aguapenApi: 'http://127.0.0.1:8000/api/',
+  aguapenApi: 'https://content-manifestation-production.up.railway.app/api/',
   //aguapenApi: 'http://192.168.1.30:80/api/',
 
   //imageApi: 'https://aguapenrestapi-arvnq.ondigitalocean.app',
-  imageApi: 'http://127.0.0.1:8000',
+  imageApi: 'https://content-manifestation-production.up.railway.app/api/',
   //imageApi: 'http://192.168.1.30:80/',
   //0923827604
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,13 @@
 export const environment = {
 
- // aguapenApi: 'https://aguapenrestapi-arvnq.ondigitalocean.app/api/',
+//aguapenApi: 'https://aguapenrestapi-arvnq.ondigitalocean.app/api/',
+  //aguapenApi: 'http://127.0.0.1:8000/api/',
+  aguapenApi: 'https://content-manifestation-production.up.railway.app/api/',
+  //aguapenApi: 'http://192.168.1.30:80/api/',
+
+  //imageApi: 'https://aguapenrestapi-arvnq.ondigitalocean.app',
+  imagenApi: 'https://content-manifestation-production.up.railway.app/api/',
+  //imageApi: 'http://192.168.1.30:80/',
+  //0923827604
 
 };
