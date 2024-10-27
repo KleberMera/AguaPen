003458b530571b29673814394895b16019e9fdd2
KleberMera <klebermera2016@gmail.com>
2024-10-26 23:16:42 -0500

    assing in progress

--- src/app/pages/tics/permissions/components/panel-assing/panel-assing.component.ts
+++ src/app/pages/tics/permissions/components/panel-assing/panel-assing.component.ts
@@ -1,15 +1,67 @@
-import { Component, input, Input } from '@angular/core';
+import { Component, inject, input, Input, OnInit, signal } from '@angular/core';
 import { UserAttributes } from '../../../../../models/users.model';
+import { lsUserPermissions } from '../../../../../models/auth.model';
+import { PermisosService } from '../../../../../services/auth/permisos.service';
+import { toast } from 'ngx-sonner';
+import { HandleErrorService } from '../../../../../services/gen/handle-error.service';
+import { groupModulesByName } from '../panel-update/panle-update.imports';
 
 @Component({
   selector: 'app-panel-assing',
   standalone: true,
   imports: [],
   templateUrl: './panel-assing.component.html',
-  styleUrl: './panel-assing.component.scss'
+  styleUrl: './panel-assing.component.scss',
 })
 export class PanelAssingComponent {
- user = input.required<UserAttributes | null>();
- key = input<string | number>(); // Input para la key
+  user = input.required<UserAttributes | null>();
+  protected listAllModules = signal<lsUserPermissions[]>([]);
+  protected listModulesUser = signal<lsUserPermissions[]>([]);
+  private readonly srvPermisos = inject(PermisosService);
+  private readonly srvError = inject(HandleErrorService);
+  groupedModules: Array<{ nombre_modulo: string; menus: Array<{ nombre_menu: string; opciones: lsUserPermissions[] }> }> = [];
+
+  ngOnInit(): void {
+    this.getAllModules();
+  }
+
+  async getAllModules() {
+    try {
+      const res = await this.srvPermisos.getListModulos().toPromise();
+      if (res.data) {
+        this.listAllModules.set(res.data);
+        console.log(this.listAllModules());
+        await this.getListModulesUser(this.user()!);
+        
+      }
+    } catch (error) {}
+  }
+
+  async getListModulesUser(user: UserAttributes) {
+    const userId = user.id;
+    try {
+      const res = await this.srvPermisos
+        .getListPermisosPorUsuario(userId)
+        .toPromise();
+      if (res.data) {
+        this.listModulesUser.set(res.data);
+        console.log(this.listModulesUser());
+        const missingModules = this.getMissingModules();
+        this.groupedModules = groupModulesByName(missingModules);
+      }
+    } catch (error) {
+      const storeError = this.srvError.getError().error.message;
+      toast.error(storeError);
+    }
+  }
+
+  getMissingModules() {
+  // Obtener los ids de opciones en los módulos del usuario
+  const userModuleIds = new Set(this.listModulesUser().map(module => module.opcion_id));
+  // Filtrar los módulos de `listAllModules` que no están en `listModulesUser`
+  const missingModules = this.listAllModules().filter(module => !userModuleIds.has(module.opcion_id));
+
+  return missingModules;
+}
 
 }
